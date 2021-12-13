4630472160c0f301cff0c4391551616c2a8a2e54
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-12-12 19:28:10 -0800

    Changed fs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const notesRouter = require('./notes');
 
-
 const app = express();
 app.use('/notes', notesRouter);
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+// const api = require('./routes/index.js');
+const uuid = require('./helpers/uuid');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
 
 const PORT = 3001;
 
@@ -9,7 +12,6 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', api);
 
 app.use(express.static('public'));
 
@@ -22,6 +24,46 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET Route for notes API
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for note`);
+
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  console.info(`${req.method} request received to delete a note`);
+}) 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
